2be4b74d01d8619a05ddd72b01cc96742f77d75d
efojug <lidudu890890@163.com>
2025-02-10 15:06:03 +0800

    server

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:2.3.0")
+    implementation("io.ktor:ktor-server-netty:2.3.0")
+    implementation("io.ktor:ktor-server-websockets:2.3.0")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,44 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = 4380, host = "0.0.0.0") {
+        install(WebSockets) {
+            pingPeriod = Duration.ofSeconds(10)
+            timeout = Duration.ofSeconds(10)
+            maxFrameSize = Long.MAX_VALUE
+            masking = false
+        }
+        routing {
+            webSocket("/chat") {
+                println("Client connected: ${this.call.request.origin.remoteHost}")
+
+            }
+        }
+    }
+}
+
+object ChatServer {
+    private val sessions = mutableSetOf<DefaultWebSocketServerSession>()
+    fun addSession(session: DefaultWebSocketServerSession) {
+        sessions.add(session)
+    }
+
+    fun removeSession(session: DefaultWebSocketServerSession) {
+        sessions.remove(session)
+    }
+
+    suspend fun broadcast(message: String, sender: DefaultWebSocketServerSession) {
+        sessions.forEach { session ->
+            if (session != sender) {
+                session.send(Frame.Text(message))
+            }
+        }
+    }
 }
